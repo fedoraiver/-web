d7277dbe9b2a038add3b46843cfa8fc8757a7ba1
fedoraiver <cck1772416022@gmail.com>
2023-08-12 16:40:06 +0800

    feat:get router right

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,47 @@
 <template>
   <div id="app">
-    <router-view />
+    <!-- top bar -->
+    <div
+      style="
+        height: 60px;
+        line-height: 60px;
+        background-color: white;
+        margin-bottom: 2px;
+      "
+    >
+      <img
+        src="@/assets/logo.png"
+        alt=""
+        style="width: 40px; position: relative; top: 16px; left: 20px"
+      />
+      <span style="margin-left: 24px; font-size: 24px">矩阵计算</span>
+    </div>
+
+    <!-- side bar and main body -->
+    <div style="display: flex">
+      <!-- side bar -->
+      <div
+        style="
+          width: 200px;
+          min-height: calc(100vh - 62px);
+          overflow: hidden;
+          margin-right: 2px;
+          background-color: white;
+        "
+      >
+        <el-menu :default-active="$router.path" router class="el-menu-demo">
+          <el-menu-item index="/">首页</el-menu-item>
+          <el-submenu>
+            <template slot="title">关于页面</template>
+            <el-menu-item index="/about">关于详情</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
+
+      <!-- main body -->
+      <div style="flex: 1; background-color: white">
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
