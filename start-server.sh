b711d823d4ab310a3652c1ac60034177c00d4589
fedoraiver <cck1772416022@gmail.com>
2023-12-15 21:07:24 +0800

    HITsz->HITSZ

--- start-server.sh
+++ start-server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 运行 npm run server
-PORT=49153 npm run serve --prefix /Users/tangdanyu/Desktop/HITsz/大一立项/project/vue &
+PORT=49153 npm run serve --prefix /Users/tangdanyu/Desktop/HITSZ/大一立项/project/vue &
 
 # 等待一段时间，确保服务器已经启动
 sleep 5
 
 # 运行 node server.js
-node /Users/tangdanyu/Desktop/HITsz/大一立项/project/vue/server.js
+node /Users/tangdanyu/Desktop/HITSZ/大一立项/project/vue/server.js
 
